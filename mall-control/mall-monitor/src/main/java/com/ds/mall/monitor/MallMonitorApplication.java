7374508e554f7e416b11214e6ea8380c748ae504
zhoujian <zhoujian@newtank.cn>
2019-01-04 15:52:36 +0800

    ds-mall

---
+++ mall-control/mall-monitor/src/main/java/com/ds/mall/monitor/MallMonitorApplication.java
@@ -0,0 +1,21 @@
+package com.ds.mall.monitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author tb
+ * @date 2019/1/4 15:43
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class MallMonitorApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(MallMonitorApplication.class).web(WebApplicationType.SERVLET).run(args);
+    }
+}
