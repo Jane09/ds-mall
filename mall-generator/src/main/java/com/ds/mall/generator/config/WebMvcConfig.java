1b50141e5f667918cd19f2722673f9e987af3c3a
zhoujian <zhoujian@newtank.cn>
2019-01-07 16:22:36 +0800

    ds-mall

--- mall-generator/src/main/java/com/ds/mall/generator/config/WebMvcConfig.java
+++ mall-generator/src/main/java/com/ds/mall/generator/config/WebMvcConfig.java
@@ -2,15 +2,19 @@ package com.ds.mall.generator.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 /**
+ * 其中默认配置的 /** 映射到 /static （或/public、/resources、/META-INF/resources）
+ * 其中默认配置的 /webjars/** 映射到 classpath:/META-INF/resources/webjars/
  * @author tb
  * @date 2019/1/7 15:37
  */
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
     @Override
