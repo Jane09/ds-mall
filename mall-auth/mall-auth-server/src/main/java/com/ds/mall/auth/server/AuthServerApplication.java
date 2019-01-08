4b8b65d7d4d1533aa17223789a289cca35047037
zhoujian <zhoujian@newtank.cn>
2019-01-08 10:20:27 +0800

    ds-mall

--- mall-auth/mall-auth-server/src/main/java/com/ds/mall/auth/server/AuthServerApplication.java
+++ mall-auth/mall-auth-server/src/main/java/com/ds/mall/auth/server/AuthServerApplication.java
@@ -1,8 +1,21 @@
 package com.ds.mall.auth.server;
 
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * @author tb
  * @date 2019/1/7 19:02
  */
+@SpringBootApplication
+@EnableDiscoveryClient
 public class AuthServerApplication  {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(AuthServerApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
 }
