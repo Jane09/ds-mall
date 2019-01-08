ea022bd610cc6d7df5da400fcfce1c4dfe25f388
zhoujian <zhoujian@newtank.cn>
2019-01-08 10:16:44 +0800

    ds-mall

--- mall-control/mall-monitor/src/main/java/com/ds/mall/monitor/MallMonitorApplication.java
+++ mall-control/mall-monitor/src/main/java/com/ds/mall/monitor/MallMonitorApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author tb
@@ -13,6 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAdminServer
+@Configuration
 public class MallMonitorApplication {
 
     public static void main(String[] args) {
