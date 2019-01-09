0469c96cea6eb2288a34fea8b58626c0d713ddc5
zhoujian <zhoujian@newtank.cn>
2019-01-09 17:46:29 +0800

    ds-mall

---
+++ mall-uid/src/main/java/com/ds/mall/uid/Mid.java
@@ -0,0 +1,15 @@
+package com.ds.mall.uid;
+
+import java.lang.annotation.*;
+
+/**
+ * @author tb
+ * @date 2019/1/9 17:45
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface Mid {
+
+}
