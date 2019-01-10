50f55ef48ca97e64b85f0c55112e2f8c6369b383
zhoujian <zhoujian@newtank.cn>
2019-01-10 16:02:17 +0800

    ds-mall

--- mall-backend/mall-backend-api/src/main/java/com/ds/mall/backend/controller/MenuController.java
+++ mall-backend/mall-backend-api/src/main/java/com/ds/mall/backend/controller/MenuController.java
@@ -1,8 +1,13 @@
 package com.ds.mall.backend.controller;
 
+import com.ds.mall.backend.entity.Menu;
+import com.ds.mall.backend.service.MenuService;
+import com.ds.mall.common.controller.AbstractBaseController;
+
 /**
  * @author tb
  * @date 2019/1/10 16:00
  */
-public class MenuController {
+public class MenuController extends AbstractBaseController<MenuService, Menu> {
+
 }
--- mall-backend/mall-backend-api/src/main/java/com/ds/mall/backend/service/MenuService.java
+++ mall-backend/mall-backend-api/src/main/java/com/ds/mall/backend/service/MenuService.java
@@ -1,10 +1,16 @@
 package com.ds.mall.backend.service;
 
+import com.ds.mall.backend.entity.Menu;
+import com.ds.mall.backend.mapper.MenuMapper;
 import com.ds.mall.common.service.AbstractBaseService;
+import org.springframework.stereotype.Service;
 
 /**
  * @author tb
  * @date 2019/1/10 15:55
  */
-public class MenuService extends AbstractBaseService {
+@Service
+public class MenuService extends AbstractBaseService<MenuMapper, Menu> {
+
+
 }
