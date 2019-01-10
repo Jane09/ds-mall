5b4073a8f1cffcd07f0086c7a6e1e6c0cb9d56cf
zhoujian <zhoujian@newtank.cn>
2019-01-10 14:25:01 +0800

    ds-mall

---
+++ mall-auth/mall-auth-server/src/main/java/com/ds/mall/auth/server/controller/TokenController.java
@@ -0,0 +1,8 @@
+package com.ds.mall.auth.server.controller;
+
+/**
+ * @author tb
+ * @date 2019/1/10 14:10
+ */
+public class TokenController {
+}
---
+++ mall-auth/mall-auth-server/src/main/java/com/ds/mall/auth/server/runner/AuthServerRunner.java
@@ -0,0 +1,17 @@
+package com.ds.mall.auth.server.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author tb
+ * @date 2019/1/10 14:17
+ */
+@Configuration
+public class AuthServerRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
