b14cd9479cf9748ca50dc450ad8369b10ccb0af3
zhoujian <zhoujian@newtank.cn>
2019-01-11 16:01:20 +0800

    ds-mall

---
+++ mall-auth/mall-auth-server/src/main/java/com/ds/mall/auth/server/interceptor/ServicesInterceptor.java
@@ -0,0 +1,10 @@
+package com.ds.mall.auth.server.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * @author tb
+ * @date 2019/1/11 15:56
+ */
+public class ServicesInterceptor extends HandlerInterceptorAdapter {
+}
