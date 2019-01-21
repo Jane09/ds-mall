a6bb800d4e65e081d44b78c72c28a9bcfc8df1ac
tybest <zhoujian699@126.com>
2019-01-21 16:23:19 +0800

    ds-mall

--- mall-algorithm/src/main/java/com/ds/mall/algorithm/MultiThread.java
+++ mall-algorithm/src/main/java/com/ds/mall/algorithm/MultiThread.java
@@ -5,5 +5,29 @@ package com.ds.mall.algorithm;
  */
 public class MultiThread {
 
+    private static final int COUNT_BITS = Integer.SIZE - 3;
+    private static final int CAPACITY   = (1 << COUNT_BITS) - 1;
 
+    // runState is stored in the high-order bits
+    private static final int RUNNING    = -1 << COUNT_BITS;
+    private static final int SHUTDOWN   =  0 << COUNT_BITS;
+    private static final int STOP       =  1 << COUNT_BITS;
+    private static final int TIDYING    =  2 << COUNT_BITS;
+    private static final int TERMINATED =  3 << COUNT_BITS;
+
+    public static void main(String[] args) {
+        System.out.println(COUNT_BITS);
+        System.out.println(CAPACITY);
+        System.out.println(RUNNING);
+        System.out.println(SHUTDOWN);
+        System.out.println(STOP);
+        System.out.println(TIDYING);
+        System.out.println(TERMINATED);
+        System.out.println(runStateOf(2));
+        System.out.println(workerCountOf(2));
+
+    }
+
+    private static int runStateOf(int c)     { return c & ~CAPACITY; }
+    private static int workerCountOf(int c)  { return c & CAPACITY; }
 }
